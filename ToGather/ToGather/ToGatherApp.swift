ced79b4f654f843d15ceacccc0789c81a07cd5ea
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-06-08 09:45:49 +0900

    [Add]: Empty project file

---
+++ ToGather/ToGather/ToGatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToGatherApp.swift
+//  ToGather
+//
+//  Created by yudonlee on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct ToGatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
