5e1fbb1535bb8a46034caf07b4577139ee5ca478
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-06-08 11:39:29 +0900

    [Add] upload onboarding 3 empty views.

---
+++ ToGather/ToGather/Screens/AddingFriend/View/AddingFriend.swift
@@ -0,0 +1,20 @@
+//
+//  AddingFriend.swift
+//  ToGather
+//
+//  Created by yudonlee on 2022/06/08.
+//
+
+import SwiftUI
+
+struct AddingFriend: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddingFriend_Previews: PreviewProvider {
+    static var previews: some View {
+        AddingFriend()
+    }
+}
---
+++ ToGather/ToGather/Screens/SettingGoal/View/SettingGoal.swift
@@ -0,0 +1,20 @@
+//
+//  SettingGoal.swift
+//  ToGather
+//
+//  Created by yudonlee on 2022/06/08.
+//
+
+import SwiftUI
+
+struct SettingGoal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingGoal_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingGoal()
+    }
+}
