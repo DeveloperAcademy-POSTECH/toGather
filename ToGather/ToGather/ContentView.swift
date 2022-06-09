6677a5b23ee02d4c3f297e86f21916a2a1da2e7b
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-06-09 11:05:22 +0900

    [Add]: upload new empty project file

---
+++ ToGather/ToGather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToGather
+//
+//  Created by yudonlee on 2022/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
