87035f1c357ecf2ce4603c26d3752dfc25a6a332
yeongwoo_Cho <jyy0223@naver.com>
2022-06-09 16:48:10 +0900

    [Add] GoalSetting file

---
+++ ToGather/ToGather/Screens/GoalSetting/View/GoalSetting.swift
@@ -0,0 +1,20 @@
+//
+//  GoalSetting.swift
+//  ToGather
+//
+//  Created by yeongwoocho on 2022/06/09.
+//
+
+import SwiftUI
+
+struct GoalSetting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GoalSetting_Previews: PreviewProvider {
+    static var previews: some View {
+        GoalSetting()
+    }
+}
