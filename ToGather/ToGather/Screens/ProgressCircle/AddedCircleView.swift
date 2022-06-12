0d77748d3f75e8a843fae6663d9d8698ab523172
Miller <eotjd8721@naver.com>
2022-06-12 22:24:08 +0900

    [Add] add mainview addedButton

---
+++ ToGather/ToGather/Screens/ProgressCircle/AddedCircleView.swift
@@ -0,0 +1,35 @@
+//
+//  AddedCircleView.swift
+//  ToGather
+//
+//  Created by DaeSeong on 2022/06/12.
+//
+
+import SwiftUI
+
+struct AddedCircleView: View {
+    
+    let color: Color
+    var body: some View {
+        ZStack {
+        Circle()
+            .frame(width: 80, height: 80)
+            .foregroundColor(color.opacity(0.05))
+            Image(systemName: "plus")
+                .resizable()
+                .frame(width: 28, height: 28)
+                .foregroundColor(.basicRed)
+        
+        }
+       // .frame(width: <#T##CGFloat?#>, height: <#T##CGFloat?#>, alignment: <#T##Alignment#>)
+
+    }
+}
+
+struct AddedCircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddedCircleView(color: .basicRed)
+            .previewLayout(.sizeThatFits)
+
+    }
+}
