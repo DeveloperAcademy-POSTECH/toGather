0ff715dc262fd8fbf00179feeccb312c07c0f003
limhyoseok <nonamedoud@gmail.com>
2022-06-13 10:05:22 +0900

    [bug] addSettingPeriod

---
+++ ToGather/ToGather/Screens/SettingPeriod/SettingPeriod.swift
@@ -0,0 +1,20 @@
+//
+//  SettingPeriod.swift
+//  ToGather
+//
+//  Created by hyo on 2022/06/13.
+//
+
+import SwiftUI
+
+struct SettingPeriod: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingPeriod_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingPeriod()
+    }
+}
