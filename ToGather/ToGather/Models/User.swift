3e5ff42a09424825849a7834ab9fd166e75824ec
limhyoseok <nonamedoud@gmail.com>
2022-06-15 16:25:07 +0900

    User Struct 구조 변경

--- ToGather/ToGather/Models/User.swift
+++ ToGather/ToGather/Models/User.swift
@@ -13,6 +13,8 @@ struct User {
     let nickname: String
     let creationDate: Date
     //let uuid: String  // UUID획득코드 : UIDevice.current.identifierForVendor!.uuidString
-    var isAlarmOn: Bool? = false
+    
+    var isAlarmOn: Bool?
     // 친구목록은 파이어베이스에 docoument 만들어서 그안에 친구uid넣으면 될거 같습니다.
+    var saveInfo: Saving
 }
