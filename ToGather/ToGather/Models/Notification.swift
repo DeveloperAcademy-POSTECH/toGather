e7c2cec80f3765b1f223251f3df95cf716e1ea16
Miller <eotjd8721@naver.com>
2022-06-16 00:57:18 +0900

    [Chore] code refatoring for easy reading

--- ToGather/ToGather/Models/Notification.swift
+++ ToGather/ToGather/Models/Notification.swift
@@ -32,4 +32,3 @@ enum NotificationType: Int, Decodable {
         }
     }
 }
-
--- ToGather/ToGather/Models/User.swift
+++ ToGather/ToGather/Models/User.swift
@@ -12,7 +12,6 @@ struct User : Codable {
     var id: String? // uuid인데 이후 Identifiable프로토콜 구현을 위해 id로 명명
     let nickname: String
     let creationDate: Date
-    //let uuid: String  // UUID획득코드 : UIDevice.current.identifierForVendor!.uuidString
     var isAlarmOn: Bool? = false
     // 친구목록은 파이어베이스에 docoument 만들어서 그안에 친구uid넣으면 될거 같습니다.
 }
