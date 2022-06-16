5e5fc25819affb71abf5a6d6e3e634842e23d935
limhyoseok <nonamedoud@gmail.com>
2022-06-17 08:51:54 +0900

    numberformat 이름 변경

--- ToGather/ToGather/Global/Service/MoneyLogic.swift
+++ ToGather/ToGather/Global/Service/MoneyLogic.swift
@@ -12,7 +12,7 @@ func getSaveAmountOfWeek(productPrice : Double, goalWeek: Int) -> Double {
 }
 
 /// 만원 단위의 Double 타입 값을 넣어주면 콤마 넣어서 전체 숫자 반환 ex) 10 -> 100,000
-func numberFormat(value: Double) -> String {
+func numberFormatUnitOfManwon(value: Double) -> String {
     let numberFormatter = NumberFormatter()
     numberFormatter.numberStyle = .decimal    
     return numberFormatter.string(from: NSNumber(value: Int(value * 10000))) ?? "error"
