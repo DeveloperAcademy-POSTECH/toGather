2932509a864ad96295fe02bf0fba1c3795ba5fcb
limhyoseok <nonamedoud@gmail.com>
2022-06-17 00:24:24 +0900

    변화된 saving 객체에 맞게 인자 제거

--- ToGather/ToGather/Global/Service/UserViewModel.swift
+++ ToGather/ToGather/Global/Service/UserViewModel.swift
@@ -12,8 +12,9 @@ import UIKit
 final class UserViewModel: ObservableObject {
     @Published var userData = User(id: "", nickname: "", creationDate: "", isAlarmOn: true,
                                    saveInfo: Saving(goalProduct: Product(productName: "", productPrice: 0, imageUrl: ""),
-                                                    goalWeeks: 1, startDate: "", savingDayOfTheWeek: "", weekInfo: [ThisWeek]()))
+                                                    goalWeeks: 1, savingDayOfTheWeek: "", weekInfo: [ThisWeek]()))
     @Published var friendUids: [String] = []
+    
     
     /// goal-setting 뷰에서 사용, savingData 인스턴스에 목표 product 추가
     func addProduct(product: Product) {
