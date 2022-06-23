e31243dedf8fb39ccd119d3ea42c70cd26744239
limhyoseok <nonamedoud@gmail.com>
2022-06-24 07:02:00 +0900

    [fix] saving에 리팩토링 적용

--- ToGather/ToGather/Models/Saving.swift
+++ ToGather/ToGather/Models/Saving.swift
@@ -10,10 +10,9 @@ import Firebase
 
 struct Saving : Codable {
     var goalProduct: Product
-//    let uid: String
+
     var goalWeeks: Int // 처음 설정한 목표 저축 기간. ~주
     
-    // MARK: 일단 테스트 용으로 값을 입력 받음. 나중에는 자동으로 계산.
     var startDate: String // 첫번째 저축 시작 날짜.
 
     var savingDayOfTheWeek: String // 저축 요일
@@ -42,13 +41,14 @@ struct Saving : Codable {
         self.weekInfo = weekInfo
     }
     
+    var currentWeekEndDate: Date {getCurrentWeekEndDate(currentWeek: currentWeek, firstSavingDate: startDate)}
     
-    var deadLine: String {getRemainTime(firstSavingDate: startDate)}
+    var deadLine: String {getRemainTime(currentWeekEndDate: currentWeekEndDate)}
     
     var lastDate: String {getLastSavingDate(firstSavingDate: startDate, totalWeek: totalWeek)}
         
     /// 매주 저축해야하는 금액
-    var savingAmountOfWeek: Double {getSaveAmountOfWeek(productPrice: goalProduct.productPrice, goalWeek: goalWeek)}
+    var savingAmountOfWeek: Double {getSaveAmountOfWeek(productPrice: goalProduct.productPrice, goalWeek: goalWeeks)}
     
     /// 목표 저축 금액(전체)
     var goalSavingAmount: Double {savingAmountOfWeek * Double(goalWeeks)}
@@ -56,8 +56,10 @@ struct Saving : Codable {
     /// 현재까지 전체 저축 금액
     var totalSavingAmount: Double {savingAmountOfWeek * Double(totalSavedNum)}
     
+//    var isSavedCurrentWeek: Bool {weekInfo[currentWeek - 1].didSave == true}
+    
     /// 저축 회차 << 핵심 변수.
-    var currentWeek: Int { getCurrentWeek(from: startDate) }
+    var currentWeek: Int { getCurrentWeek(from: startDate, weekInfo: weekInfo) }
     
     /// 전체 기간
     var totalWeek: Int {totalFailedNum + goalWeeks}
