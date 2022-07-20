134e36cd0e6a4dcde6fdc9f8bca18ce87bbea55f
limhyoseok <nonamedoud@gmail.com>
2022-07-20 15:21:25 +0900

    fix : isActive 받도록 수정

--- ToGather/ToGather/Global/Service/CustomNavigationLink.swift
+++ ToGather/ToGather/Global/Service/CustomNavigationLink.swift
@@ -10,17 +10,43 @@ import SwiftUI
 struct CustomNavigationLink<DestinationView, ContentView>: View where ContentView: View, DestinationView: View {
     
     var destination: DestinationView
-    var isActive: Binding<Bool> = .constant(true)
+    @Binding var isActive: Bool
     var label: () -> ContentView
+    var hasIsActive: Bool
+    
+    init(destination: DestinationView, isActive: Binding<Bool>, label: @escaping () -> ContentView) {
+        self.destination = destination
+        self._isActive = isActive
+        self.label = label
+        self.hasIsActive = true
+    }
+    
+    init(destination: DestinationView, label: @escaping () -> ContentView) {
+        self.destination = destination
+        self.label = label
+        self._isActive = .constant(true)
+        self.hasIsActive = false
+    }
     
     var body: some View {
-        NavigationLink(isActive: isActive, destination: {
-            destination
-                .navigationBarItems(leading: ArrowBackButton())
-                .navigationBarBackButtonHidden(true)
-        }){
-            label()
+        if hasIsActive{
+            NavigationLink(isActive: $isActive, destination: {
+                destination
+                    .navigationBarItems(leading: ArrowBackButton())
+                    .navigationBarBackButtonHidden(true)
+            }){
+                label()
+            }
+        } else {
+            NavigationLink(destination: {
+                destination
+                    .navigationBarItems(leading: ArrowBackButton())
+                    .navigationBarBackButtonHidden(true)
+            }){
+                label()
+            }
         }
+      
     }
     
     // let button = Button {~} 할때는 빌드는 되지만 뒤로가기가 되지 않았음. 아마도 초기화 시점이 문제지 않을까 싶다.
@@ -40,29 +66,47 @@ struct CustomNavigationLink<DestinationView, ContentView>: View where ContentVie
 }
 
 struct CustomNavigationLink_Previews: PreviewProvider {
-    
     struct NormalThirdPage: View {
+        @Binding var isActive: Bool
+        
         var body: some View {
-            Text("세번째 페이지 컨텐츠 내용")
-                .navigationTitle("세번째 페이지")
+            VStack {
+                Text("세번째 페이지 컨텐츠 내용")
+                    .navigationTitle("세번째 페이지")
+                Button {
+                    isActive = false
+                } label: {
+                    Text("click")
+                }
+            }
+            
         }
     }
     
     struct TempSecondPage: View {
+        @Binding var isActive: Bool
+        
         var body: some View {
-            CustomNavigationLink(destination: NormalThirdPage()) {
-                Text("두번째 페이지 컨텐츠 내용")
+            CustomNavigationLink(destination: NormalThirdPage(isActive: $isActive)) {
+                Text("두번째 페이지 컨텐츠 내용, isActive: \(isActive ? "true" : "false")")
                     .navigationTitle("두번째 페이지")
             }
         }
     }
     
-    static var previews: some View {
-        NavigationView {
-            CustomNavigationLink(destination: TempSecondPage(), isActive: .constant(false)) {
-                Text("첫번째 페이지")
+    struct TempFirstPage: View {
+        @State var isActive = false
+        
+        var body: some View {
+            NavigationView {
+                CustomNavigationLink(destination: TempSecondPage(isActive: $isActive), isActive: $isActive) {
+                    Text("첫번째 페이지 isActive: \(isActive ? "true" : "false")")
+                }
             }
         }
     }
+    
+    static var previews: some View {
+        TempFirstPage()
+    }
 }
-
