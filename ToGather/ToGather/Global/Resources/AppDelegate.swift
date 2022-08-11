4196da1986e5cff33e85fdef05a7312d6e5dd1b6
Miller <eotjd8721@naver.com>
2022-08-11 22:06:11 +0900

    [FEAT] temp3

--- ToGather/ToGather/Global/Resources/AppDelegate.swift
+++ ToGather/ToGather/Global/Resources/AppDelegate.swift
@@ -8,41 +8,37 @@
 import SwiftUI
 import UIKit
 import FirebaseMessaging
-class AppDelegate: NSObject, UIApplicationDelegate,MessagingDelegate {
-    
+class AppDelegate: NSObject, UIApplicationDelegate {
+
     var userToken: String = ""
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 
         Messaging.messaging().delegate = self
           UNUserNotificationCenter.current().delegate = self
-        
+
           UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, _ in
               guard success else {
                   return
               }
-              
+
               print("Success in APNS registry")
           }
-        
+
           application.registerForRemoteNotifications()
-        
-        
+
         return true
     }
     
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        messaging.token { token, _ in
-            guard let token = token else {
-                return
-            }
-            self.userToken = token
-            print("Token: \(token)")
-        }
-    }
-     
-}
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("가져올 데이터가 있음을 나타내는 원격 알림이 도착했음을 앱에 알림")
 
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+
+
+}
 
 extension AppDelegate : UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter,willPresent notification: UNNotification,withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
@@ -52,4 +48,18 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter,didReceive response: UNNotificationResponse,withCompletionHandler completionHandler: @escaping () -> Void) {
         completionHandler()
     }
+    
+}
+
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        messaging.token { token, _ in
+            guard let token = token else {
+                return
+            }
+            self.userToken = token
+            print("Token: \(token)")
+        }
+    }
+
 }
--- ToGather/ToGather/Global/Service/PushNotificationManager.swift
+++ ToGather/ToGather/Global/Service/PushNotificationManager.swift
@@ -7,17 +7,46 @@
 
 import Foundation
 
-struct PushNotificationManager {
+//struct PushNotificationManager {
+//    func sendPushNotification(to token: String, title: String, body: String) {
+//        let server_key = ""
+//        let urlString = "https://fcm.googleapis.com/fcm/send"
+//        let url = NSURL(string: urlString)!
+//        let paramString: [String : Any] = ["to" : token,
+//                                           "notification" : ["title" : title, "body" : body],
+//                                           "data" : ["user" : "test_id"]
+//        ]
+//
+//
+//    }
+//
+//}
+
+class PushNotificationManager {
     func sendPushNotification(to token: String, title: String, body: String) {
-        let server_key = ""
+        let server_key = "AAAA4DyC34g:APA91bFrJPkuapSLQXN92-ry6UDIhULukpP_nsRJyfnI_-zgGVIcuisReSdHjav520_sKVe37G7UibBmdFNunRtNaWbac3A72Sb30ZaGgDC4MhSbGQKv51t_mfmVInHOhD_Yv4PrHy4r"
         let urlString = "https://fcm.googleapis.com/fcm/send"
         let url = NSURL(string: urlString)!
         let paramString: [String : Any] = ["to" : token,
                                            "notification" : ["title" : title, "body" : body],
                                            "data" : ["user" : "test_id"]
         ]
-        
-    
+        let request = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject:paramString, options: [.prettyPrinted])
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("key=\(server_key)", forHTTPHeaderField: "Authorization")
+        let task =  URLSession.shared.dataTask(with: request as URLRequest)  { (data, response, error) in
+            do {
+                if let jsonData = data {
+                    if let jsonDataDict  = try JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: AnyObject] {
+                        NSLog("Received data:\n\(jsonDataDict))")
+                    }
+                }
+            } catch let err as NSError {
+                print(err.debugDescription)
+            }
+        }
+        task.resume()
     }
-
 }
