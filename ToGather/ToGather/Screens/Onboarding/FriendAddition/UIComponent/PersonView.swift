ee6c70612a3b0d08072907cd5ca5779ed4975f2b
Miller <eotjd8721@naver.com>
2022-08-11 12:03:57 +0900

    [CHORE] 코드 리팩토링

---
+++ ToGather/ToGather/Screens/Onboarding/FriendAddition/UIComponent/PersonView.swift
@@ -0,0 +1,20 @@
+//
+//  PersonView.swift
+//  ToGather
+//
+//  Created by DaeSeong on 2022/08/11.
+//
+
+import SwiftUI
+
+struct PersonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PersonView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonView()
+    }
+}
